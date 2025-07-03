1ae958dbb74476f664d251e1642177f6a08367f2
caucashanus <caucashanus@gmail.com>
2025-07-03 15:51:57 +0200

    Update app.js

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ import { getDatabase, ref, push, onValue, set } from "https://www.gstatic.com/fi
 const firebaseConfig = {
   apiKey: "AIzaSyBerVfLy960LY0vw0d0HYwVfQKK_gmtyVI",
   authDomain: "nakupni-seznam-e6d66.firebaseapp.com",
-  databaseURL: "databaseURL: "https://nakupni-seznam-e6d66-default-rtdb.firebaseio.com",
+  databaseURL: "https://nakupni-seznam-e6d66-default-rtdb.firebaseio.com", 
   projectId: "nakupni-seznam-e6d66",
   storageBucket: "nakupni-seznam-e6d66.appspot.com",
   messagingSenderId: "49601954404",
   appId: "1:49601954404:web:1351599fea2f676f921571"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const db = getDatabase(app);
