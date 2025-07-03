588a147af42fd577fbf9af64a6ec1d66a1cef1b5
caucashanus <caucashanus@gmail.com>
2025-07-03 15:31:43 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getDatabase, ref, push, onValue, set } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-database.js";
+
 const firebaseConfig = {
   apiKey: "AIzaSyBerVfLy960LY0vw0d0HYwVfQKK_gmtyVI",
   authDomain: "nakupni-seznam-e6d66.firebaseapp.com",
@@ -7,3 +10,10 @@ const firebaseConfig = {
   messagingSenderId: "49601954404",
   appId: "1:49601954404:web:1351599fea2f676f921571"
 };
+
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const listRef = ref(db, "nakup");
+
+
+
